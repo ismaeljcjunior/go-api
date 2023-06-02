f9f511e671bb2a3fcc8e091c14a22f7c370a9761
Ismael Junior <ismaeljcjunior@gmail.com>
2023-06-02 17:06:44 -0300

    feat

--- router/router.go
+++ router/router.go
@@ -1,18 +1,12 @@
 package router
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
 func Initialize() {
+
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	initializeRoutes(router)
+
 	router.Run(":8080")
 }
